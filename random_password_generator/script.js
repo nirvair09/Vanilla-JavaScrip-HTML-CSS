68285da05e16e86668c9cd3b3da4c51bd656008e
nirvair09 <chandradeep262003@gmail.com>
2026-03-06 06:04:44 +0530

    Add initial implementation of Random Password Generator with HTML, CSS, and JavaScript

---
+++ random_password_generator/script.js
@@ -0,0 +1,22 @@
+const generateBtn = document.querySelector(".btn");
+const input = document.querySelector(".password");
+const copyBtn = document.querySelector(".copy-btn");
+
+
+const generatePassword = () => {
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let password = "";
+    for (let i = 0; i < 14; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+    input.value = password;
+}
+
+generateBtn.addEventListener("click", generatePassword);
+
+
+copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(input.value);
+    alert("Password Copied to Clipboard!");
+})
