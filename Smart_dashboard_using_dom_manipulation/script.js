562f5bfc42f814f0b344e585a57f30130bf9bf26
nirvair09 <chandradeep262003@gmail.com>
2026-04-28 10:11:58 +0530

    feat: initialize smart dashboard with responsive navbar and interactive tab switching functionality

--- Smart_dashboard_using_dom_manipulation/script.js
+++ Smart_dashboard_using_dom_manipulation/script.js
@@ -6,3 +6,22 @@ const menu = document.getElementById("menu");
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
 })
+
+
+const tabs = document.querySelectorAll(".tab");
+const list = document.getElementById("list")
+
+function render(e) {
+    list.innerHTML = `<h3>${e} Content</h3>`
+}
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        tabs.forEach(t => t.classList.remove("active"))
+        tab.classList.add("active");
+
+        render(tab.innerHTML);
+
+    })
+})
+
